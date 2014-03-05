99d0ed6f624952d9ba68e3e8236f36d0edf59e25
institution <sta256@gmail.com>
2014-03-05 19:27:59 +0100

    work on ai

--- ai.h
+++ ai.h
@@ -7,59 +7,259 @@
 
 
 namespace col {
-	
-	/*
-	
-	struct Move {
-		Icon::Id icon_id;
-		Dir dir;
-		Move(Icon::Id const& iid, Dir const& dirr);
-	};		
-
-	struct Attack {
-		Icon::Id icon_id;
-		Dir dir;			
-		Attack(Icon::Id const& iid, Dir const& dirr);
-	};
-
-	struct Turn {
-
-	};
-
-	
-	
-	get_current_player
+	template<typename Action>
+	struct Node {
+		Action *action;
+		float win, total;
+		
+		Node *parent;
+		Node *left_child;
+		Node *right_neigh;
+					
+		Node(Action *action_, float win_=0, float total_=0) {
+			action = action_;
+			win = win_; 
+			total = total_;
 			
-	play_random_game
-		int16 MAX_TURN = 5
-
-
-		while (turn < MAX_TURN) {
-
-
+			left_child = nullptr;
+			right_neigh = nullptr;
+			parent = nullptr;
 		}
+		
+		~Node() {
+			if (action)
+				delete action;
+			
+			if (left_child)
+				delete left_child;
+			
+			if (right_neigh)
+				delete right_neigh;
+		}
+	};
 	
-	get_result(Env const& env, Player::Id pid)
-		
-		
-	void create_env(Env const& env) {
-		// copy and randomly fill hidden information
-		
+	
+	ostream& operator<<(ostream &out, Node const& n) {
+		out<<"Node("<<(*n.action)<<","<<n.win<<","<<n.total<<")";
+		//out << (format(
+		//	"Icon(id=%||, type_id=%||, x=%||, y=%||, player_id=%||)"
+		//) % t.id % t.type->id % t.x % t.y % t.player->id);
 	}
 	
 	
-	
-	
-	Action create_random_action(Env const& g) {
-		auto p = get_current_player();
+	template<typename Game>
+	struct MCTS {
+		using PlayerId = typename Game::Player::Id;
+		using NodeType = Node<typename Game::Action>;
 		
+		PlayerId player_id;
+		NodeType *root;
+		Game const& o_env;
+		
+		MCTS(Game const& env, PlayerId const& pid): o_env(env), player_id(pid) {
+			root = new Node<typename Game::Action>(nullptr);			
+		}
+		
+		~MCTS() {
+			if (root) 
+				delete root;
+		}
+		
+		void think() {
+			Action *ra = nullptr;
+			auto const& pid = player_id;
+			int const NUM_OF_PLAYERS = 2;
 			
+			NodeType *node = root;
+			
+			Env env(o_env);  // copy curr state
+			
+			// select
+			while (node->left_child) {
+								
+				float w = node->win;
+				float n = node->total;
+				float ln_t = ln(parent->total);
+				
+				NodeType *child_node = node->left_child;
+				
+				ra = create_random_action(env, pid)
+				
+				float best_value = 0;
+				do {
+					// check action already generated
+					if (ra && *ra == *(child_node->action)) {
+						delete ra;
+						ra == nullptr;
+					}							
+					
+					// find best node
+					float value = w / n + sqrt(2 * ln_t / n);
+					if (value > best_value) {
+						best_child = child_node;
+						best_value = value;
+					}
+					
+				} 
+				while (child_node = child_node->right_neigh);
+				
+				// select new action or best_node
+				if (ra) {
+					// add and select node with new action
+					node = create_child(node, ra);
+				}
+				else {
+					// select best node
+					node = best_node;
+				}
+						
+			}
+			
+			// expand
+			while (ra = create_random_action(env, pid)) {
+				env.exec(ra);
+				node = create_child(node, ra);				
+			}
+			
+			
+			// play (5 random turns ahead)
+			for (int i=0; i<5*NUM_OF_PLAYERS; ++i) {
+				while (ra = create_random_action(env, get_pid(env))) {
+					env.exec(ra);
+				}
+			}
+			
+			float result = get_result(env, player_id);
+			
+			// backprop
+			while (node) {
+				node->win += result;
+				node->total += 1.0f;
+				node = node->parent;
+			}
+			
+		}
 		
+		Action const* get_action() {
+			// return best move
+			node = root;
+			Node *child_node = root->left_child;
+			Node *best_child = nullptr;
+			float best_value = -1;
+				
+			while (child_node) {
+				float value = child_node->total;
+				if (value > best_value) {
+					best_child = child_node;
+					best_value = value;
+				}
+				child_node = child_node->right_neigh;
+			}
+			
+			// best_child becomes new root
+			extract(best_child);
+			destroy_node(root);			
+			root = best_child;			
+			return root->action;
+		}
+				
+	};
+	
+	
+	float get_result(Env const& env, Player::Id pid) {
+		map<Player::Id, float> pts;
+		
+		for (auto& x: env.players) {
+			Player& pl = x.second;
+			pts[pl.id] = 0;			
+		}
+		
+		for (auto& x: env.icons) {
+			pts[x.second->player->id] += 1;
+		}		
+		
+		float total = 0;
+		for (auto& x: pts) {
+			total += x.second;
+		}
+		
+		return pts.at(pid)/total;		
+	}
+		
+	
+	void destroy_node(Node *node) {
+		delete node;
+	}
+	
+	void create_child(Node *parent, Node *child) {
+		tmp = left_child;
+		left_child = child;
+		child.right_neigh = tmp;		
+		child.parent = parent;
+	}
+	
+	void extract(Node *node) {
+		
+		parent = node->parent;
+		assert(parent != nullptr);
+		child_node = parent->left_child;
+		while (child_node) {
+			if (node == child_node->right_neigh) {
+				child_node->right_neigh = node->right_neigh;
+				node->parent = nullptr;
+				node->right_neigh = nullptr;
+				return;
+			}					
+			child_node = child_node->right_neigh;
+		}
+		
+		assert(0); // impossible to get here
 	}
 			
-*/
 	
 		
+}
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 /*
 	template<typename T>
@@ -240,6 +440,3 @@ namespace col {
 
 
 */
-}
-
-#endif
