96aa28882442abb05ef236d1ca815f978a46e374
institution <sta256@gmail.com>
2014-05-07 12:37:34 +0200

    main [savefile] # enabled

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <boost/multi_array.hpp>
 #include <boost/format.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
 #include "col.hpp"
 #include "iomm.h"
 #include "env.h"
@@ -16,6 +19,8 @@
 #include "envgame.h"
 #include "console.h"
 #include "renderer.h"
+
+
 
 using std::unique_ptr;
 
@@ -43,8 +48,8 @@ using std::vector;
  * click to enter colony
  * button to exit colony
  * space for menu
- * 
- * 
+ *
+ *
  */
 
 namespace col{
@@ -53,7 +58,7 @@ namespace col{
 		HUMAN,
 		AI
 	};
-	
+
 	PlayerType get_player_type(const Player &p) {
 		if (p.id == 0) {
 			return HUMAN;
@@ -62,23 +67,23 @@ namespace col{
 			return AI;
 		}
 	}
-	
-	
+
+
 	// ctrl - console
 	// model - env
 	// view - renderer
 
 	// console: INPUT -> model
 	// renderer: model,console -> OUTPUT
-	// 
-	
+	//
+
 	void handle_events(sf::RenderWindow &app, Console &con) {
 		sf::Event ev;
 		while (app.pollEvent(ev)) {
 			if (ev.type == sf::Event::KeyPressed) {
 				if (ev.key.code == sf::Keyboard::Escape) {
 					app.close();
-				}				
+				}
 			}
 			else
 			if (ev.type == sf::Event::Closed) {
@@ -96,65 +101,72 @@ namespace col{
 
 
 
-int main()
+int main(int argc, char* argv[])
 {
 	using namespace col;
-	
+
 	string fname("./aaa.mp");
-	
+
 	const uint SCL = 3;
 	sf::RenderWindow app(sf::VideoMode(320 * SCL, 200 * SCL, 32), "SFML Window");
 	sf::View view(sf::FloatRect(0, 0, 320, 200));
 	app.setView(view);
-	
-	
-	
+
+
+
 	//cout << "Loading terr types...";
 	//tts = load_terr_types();
 	//cout << " " << tts.size() << " loaded." << endl;
 
 	preload_terrain();
-		
+
 	EnvGame env;
 
 	env.loads<TerrType>("./col94/terrs.csv");
 	env.loads<BuildType>("./col94/builds.csv");
 	env.loads<UnitType>("./col94/units.csv");
-	
+
 	//ifstream f(fname, std::ios::binary);
-	// 
-	//f.close();	
-    
-	env.resize({15,12});
-	env.fill(Terr{Biome::Plains, Phys::None});
-	
+	//
+	//f.close();
+
+	if (argc == 2) {
+		ifstream f(argv[1], std::ios::binary);
+		boost::archive::text_iarchive ar(f);
+		ar >> env;
+	}
+	else {
+		env.resize({15,12});
+		env.fill(Terr{Biome::Plains, Phys::None});
+	}
+
 	Console con(env);
-		
+
 	auto last_env = env.mod - 1;
 	auto last_con = con.mod - 1;
 
 	while (app.isOpen())
 	{
-		
-		
+
+
 		if ((env.mod != last_env) || (con.mod != last_con)) {
 			//cout << "RENDER:" << con.mod << ',' << env.mod << endl;
-			
-			render(app, env, con);		
-			
+
+			render(app, env, con);
+
 			last_env = env.mod;
-			last_con = con.mod;		
+			last_con = con.mod;
 		}
-		
+
 		handle_events(app, con);
-		
+
 	}
 
 	/*{
 		ofstream fo(fname, std::ios::binary);
 		io::write<Env>(fo, env);
 	}*/
-		
+
 	return EXIT_SUCCESS;
 }
 
